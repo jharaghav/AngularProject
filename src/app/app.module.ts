4c8c5d1125004a641d94b5342cbdf9ee7d26fc50
raghav <raghavjha93@gmail.com>
2020-07-15 13:32:44 +0530

    added the src for angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,8 +17,24 @@ import { OopsComponent } from './oops/oops.component';
 // ----------------------------------------------------------- //
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
-import { LoginComponent } from './login/login.component';
 // ----------------------------------------------------------- //
+
+
+// ----------------------------- for forms ----------------------//
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { P1Component } from './p1/p1.component';
+import { C1Component } from './c1/c1.component';
+import { ActionComponent } from './action/action.component';
+import { ThrillerComponent } from './thriller/thriller.component';
+import { WarComponent } from './war/war.component';
+import { RomanceComponent } from './romance/romance.component';
+import { ScifiComponent } from './scifi/scifi.component';
+import { MvnavbarComponent } from './mvnavbar/mvnavbar.component';
+import { Parent1Component } from './parent1/parent1.component';
+import { Chidl1Component } from './chidl1/chidl1.component';
+// ----------------------------- for forms ---------------------//
+
 @NgModule({
   declarations: [
     RootComponent,
@@ -34,11 +50,22 @@ import { LoginComponent } from './login/login.component';
     HomeComponent,
     OopsComponent,
     LoginComponent,
+    P1Component,
+    C1Component,
+    ActionComponent,
+    ThrillerComponent,
+    WarComponent,
+    RomanceComponent,
+    ScifiComponent,
+    MvnavbarComponent,
+    Parent1Component,
+    Chidl1Component,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [RootComponent]
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {FetchmoviesService} from '../fetchmovies.service';
+import { Component, OnInit } from '@angular/core';
+import { FetchmoviesService } from '../fetchmovies.service';
+import { CommunicatorService } from '../communicator.service';
+import { LoginService } from '../login.service';
+import { AuthService } from '../auth.service';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
@@ -8,12 +13,18 @@ import {FetchmoviesService} from '../fetchmovies.service';
 })
 export class NavbarComponent implements OnInit {
   a = 10;
-
-  constructor(public fms: FetchmoviesService) {
+  loggedin;
+  name = of([1, 2, 3, 4, 5, 6, 10, 20, 30, 40]);
+  constructor(public loginService: LoginService, public auth: AuthService) {
   }
-
   ngOnInit() {
-    console.log(this.fms.getMovies(), " From Navigator");
+    this.loggedin = sessionStorage.getItem('loginUser');
+    // i want to listen on the events call  loginService --Subscriber events ---->
+    this.loginService.getSubscriber().subscribe((data) => {
+      this.loggedin = data;
+    });
+    this.name.pipe(map(name => name.map(each => each > 10))).subscribe((result) => {
+      console.log(result);
+    });
   }
-
 }
